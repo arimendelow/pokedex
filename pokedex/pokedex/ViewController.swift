6eccfc17e4ced4bd461647a536ddc43cda8b68fd
Ari Mendelow <ari@mendelow.me>
2017-04-16 21:01:34 -0400

    first commit

---
+++ pokedex/pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Ari Mendelow on 4/16/17.
+//  Copyright © 2017 Ari Mendelow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
